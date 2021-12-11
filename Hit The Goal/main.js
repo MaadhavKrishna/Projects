2a7feb418a52e7c39f2d9e08604e917a14a34212
Maadhav Krishna.A <68543275+MaadhavKrishna@users.noreply.github.com>
2021-12-11 19:38:17 +0530

    Add files via upload

--- Hit The Goal/main.js
+++ Hit The Goal/main.js
@@ -1,40 +1,39 @@
 
 var canvas = new fabric.Canvas("myCanvas")
 
-ball_x=0;
-ball_y=0;
-hole_x=400;
-hole_x=800;
+var ball_x = 0;
+var ball_y = 0;
+var hole_x = 800;
+var hole_y = 400;
 
+var block_image_width = 5;
+var block_image_height = 5;
 
-block_image_width = 5;
-block_image_height = 5;
+var hole_obj="";
+var ball_obj="";
 
-load_img();
-new_image();
-
-function load_img(){
-	fabric.Image.fromURL("golf-h.png",function(Img){
+function load_img() {
+	fabric.Image.fromURL("golf-h.png", function(Img){
 		hole_obj = Img;
 		hole_obj.scaleToWidth(50);
 		hole_obj.scaleToHeight(50);
 		hole_obj.set({
-			top:hole_y,
-			left:hole_x
+			top: hole_y,
+			left: hole_x
 		});
 		canvas.add(hole_obj);
 	});
 	new_image();
 }
 
-function new_image(){
+function new_image() {
 	fabric.Image.fromURL("ball.png",function(Img){
 		ball_obj = Img;
 		ball_obj.scaleToWidth(50);
 		ball_obj.scaleToHeight(50);
 		ball_obj.set({
-			top:hole_y,
-			left:hole_x
+			top: ball_y,
+			left: ball_x
 		});
 		canvas.add(ball_obj);
 	});
@@ -42,67 +41,68 @@ function new_image(){
 
 window.addEventListener("keydown", my_keydown);
 
-function my_keydown(e)
-{
+function my_keydown(e) {
 	keyPressed = e.keyCode;
 	console.log(keyPressed);
 	/* Check the coordinates of the ball and hole images to finish the game. 
 	And id coordinates matches them remove ball image, 
 	display "GAME OVER!!!" 
 	and make canvas border 'red'. */
-	if((ball_x==hole_x)&&(ball_y==hole_y)){
+	if ((ball_x == hole_x) && (ball_y == hole_y)) {
 		canvas.remove(ball_obj);
-		document.getElementById("hd3").innerHTML("GAME OVER!!!");
-		document.getElementById("myCanvas").style.borderColor="red";
+		document.getElementById("hd3").innerHTML="GAME OVER!!!";
+		document.getElementById("myCanvas").style.borderColor = "red";
 	}
-	else{
-		if(keyPressed == '38')
-		{
+	else {
+		if (keyPressed == '38') {
 			up();
 			console.log("up");
 		}
-		if(keyPressed == '40')
-		{
+		if (keyPressed == '40') {
 			down();
 			console.log("down");
 		}
-		if(keyPressed == '37')
-		{
+		if (keyPressed == '37') {
 			left();
 			console.log("left");
 		}
-		if(keyPressed == '39')
-		{
+		if (keyPressed == '39') {
 			right();
 			console.log("right");
 		}
 	}
-	
-	function up()
-	{
-		
-	}
 
-	function down()
-	{
-		 // Write a code to move ball downward.
-	}
-
-	function left()
-	{
-		if(ball_x >5)
-		{
-			// Write a code to move ball left side.
+	function up() {
+		if(ball_y>=0){
+			ball_y=ball_y-10;
+			canvas.remove(ball_obj);
+			new_image();
 		}
 	}
 
-	function right()
-	{
-		if(ball_x <=1050)
-		{
-			// Write a code to move ball right side.
+	function down() {
+		if(ball_y<=440){
+			ball_y=ball_y+10;
+			canvas.remove(ball_obj);
+			new_image();
+			console.log(ball_y);
 		}
 	}
-	
+
+	function left() {
+		if (ball_x >= 0) {
+			ball_x=ball_x-10;
+			canvas.remove(ball_obj);
+			new_image();
+		}
+	}
+
+	function right() {
+		if (ball_x <= 1040) {
+			ball_x=ball_x+10;
+			canvas.remove(ball_obj);
+			new_image();
+		}
+	}
+
 }
-
