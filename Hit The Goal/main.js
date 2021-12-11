57eceb4e50b8f58dfb0e1d58e93bf1988902fe43
Maadhav Krishna.A <68543275+MaadhavKrishna@users.noreply.github.com>
2021-12-11 18:41:10 +0530

    test1

--- Hit The Goal/main.js
+++ Hit The Goal/main.js
@@ -1,20 +1,40 @@
 
-// Create canvas variable
+var canvas = new fabric.Canvas("myCanvas")
 
-//Set initial positions for ball and hole images.
+ball_x=0;
+ball_y=0;
+hole_x=400;
+hole_x=800;
 
 
 block_image_width = 5;
 block_image_height = 5;
 
 function load_img(){
-	// write code to Upload golf image on the canvas
+	fabric.Image.fromURL("golf-h.png",function(Img){
+		hole_obj = Img;
+		hole_obj.scaleToWidth(50);
+		hole_obj.scaleToHeight(50);
+		hole_obj.set({
+			top:hole_y,
+			left:hole_x
+		});
+		canvas.add(hole_obj);
+	});
 	new_image();
 }
 
-function new_image()
-{
-	// write code to Upload ball image on canvas
+function new_image(){
+	fabric.Image.fromURL("ball.png",function(Img){
+		ball_obj = Img;
+		ball_obj.scaleToWidth(50);
+		ball_obj.scaleToHeight(50);
+		ball_obj.set({
+			top:hole_y,
+			left:hole_x
+		});
+		canvas.add(ball_obj);
+	});
 }
 
 window.addEventListener("keydown", my_keydown);
@@ -27,7 +47,11 @@ function my_keydown(e)
 	And id coordinates matches them remove ball image, 
 	display "GAME OVER!!!" 
 	and make canvas border 'red'. */
-	
+	if((ball_x==hole_x)&&(ball_y==hole_y)){
+		canvas.remove(ball_obj);
+		document.getElementById("hd3").innerHTML("GAME OVER!!!");
+		document.getElementById("myCanvas").style.borderColor=red;
+	}
 	else{
 		if(keyPressed == '38')
 		{
