0291ab0a7592b8017e84d117904cb3d69469f143
Maadhav Krishna.A <68543275+MaadhavKrishna@users.noreply.github.com>
2022-01-02 11:24:28 +0530

    Add files via upload

--- The Math Quiz/quiz_game_page.js
+++ The Math Quiz/quiz_game_page.js
@@ -7,3 +7,22 @@ function addUser(){
 
     window.location="quiz_game_page.html";
 }
+
+function send(){
+    number1=document.getElementById("num1").value;
+    number2=document.getElementById("num2").value;
+    actual_answer=parseInt(num1)*parseInt(num2);
+    console.log(actual_answer);
+
+    question_number="<h4>"+number1+"X"+number2+"</h4>";
+    answer_input="<h4>Answer: <input type='text' placeholder='Please Enter Answer'></h4>";
+    check="<button class='btn btn-info'>Check</button>";
+
+    row=question_number+answer_input+check;
+
+    console.log(row);
+
+    document.getElementById("output_area").innerHTML=row;
+    document.getElementById("num1").value="";
+    document.getElementById("num2").value="";
+}
